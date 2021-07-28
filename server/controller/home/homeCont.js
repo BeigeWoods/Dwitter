4666fb6569d006c3238856598bc124a2524c43fb
BeigeWoods <juhyunkbstar@gmail.com>
2021-07-29 00:01:50 +0900

    apply socket to postTweet

--- server/controller/home/homeCont.js
+++ server/controller/home/homeCont.js
@@ -1,4 +1,5 @@
 import * as tweetData from "../../data/tweets.js";
+import { getSocketIO } from "../connection/socket.js";
 
 export async function home(req, res) {
   const username = req.query.username;
@@ -12,4 +13,5 @@ export async function postTweet(req, res) {
   const { text } = req.body;
   const tweet = await tweetData.create(text, req.userId);
   res.status(201).json(tweet);
+  getSocketIO().emit("tweets", tweet);
 }
